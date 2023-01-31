216ee242abde2550a29db8e5aae695df37e75161
김경민 <leon@kimgyungmin-ui-MacBookAir.local>
2023-01-31 20:16:02 +0900

    Rebuilding Jsfile

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -52,23 +52,3 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 
-// API
-
-// Fear & Greed API
-console.log("api test start")
-
-const config = {
-    method: "get"
-    };
-    fetch("https://api.alternative.me/fng/?limit=1", config)
-    .then(response => response.json())
-    .then(data => {
-        // console.log(data)
-        const value = data.data[0]['value'] // api value 값 가져오기
-        console.log(value)
-        document.getElementById('FearAndGreedData').innerHTML = value
-        document.getElementById('FearAndGreedDataProgressbar').value = value
-    })
-    .catch(error => console.log(error));
-
-console.log("api test end")
