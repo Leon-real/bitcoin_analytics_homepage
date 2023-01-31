86ae1e33022e026ace2fc62dcdbe62c48aca750a
김경민 <leon@kimgyungmin-ui-MacBookAir.local>
2023-01-31 19:46:46 +0900

    RollBack

---
+++ static/js/test_dashboard_data.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    
+    const test_data = window.data_set_json;
+    var testData = '안녕하세요';
+    console.log(testData);
+    console.log(test_data);
+    
+
+});
