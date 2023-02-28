606bd0f6a1ae661dd8ac8f54f2ff8b02db53ea9f
Leon <tutmr999@gmail.com>
2023-02-28 18:36:36 +0900

    add bitcoin current price on summary layout

--- except_python/static/js/main_dashboard_data.js
+++ except_python/static/js/main_dashboard_data.js
@@ -40,7 +40,7 @@ function upbit_data_processing(upbit_data) {
         if (upbit_data.code.replace("KRW-",'') == value && upbit_data.type == 'ticker'){ // 키값이 같을 경우
             temp_name = value.replace('USDT',"").replace("KRW-","");
             if (temp_name == 'BTC') {
-                $('.current_bitcoin_price').text(upbit_data['trade_price'])
+                $('#current_bitcoin_price').text(upbit_data['trade_price'])
             };
             // console.log(value, upbit_data['trade_price'], upbit_data['change'])
             temp_single_data_set[temp_name] = upbit_data['trade_price']
