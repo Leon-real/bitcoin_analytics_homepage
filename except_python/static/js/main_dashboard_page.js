56ef8d784ee0136690792cc037cfad6fe8223b50
Leon <tutmr999@gmail.com>
2023-03-09 20:22:09 +0900

    Add preminum searching tools

--- except_python/static/js/main_dashboard_page.js
+++ except_python/static/js/main_dashboard_page.js
@@ -74,28 +74,30 @@ setInterval(function () {
         }
     });
     // 김치 프리미엄 테이블 값이 0인 경우 숨김 처리
-    // (function() {
-    //     var table_set = $('#coin_premium_lists_table')[0]
-    //     var rowList = table_set.rows;
-    //     // console.log(rowList)
-    //     for (i=1; i<rowList.length; i++) {
-    //         var row = rowList[i];     //thead 부분을 제외하기 위해 i가 1부터 시작됩니다.
-    //         // var tdsNum = row.childElementCount;     //아래 for문에서 사용하기 위해 row 하위에 존재하는 td의 갯수를 구합니다.
+    (function() {
+        if ($('#premium_search').val() == '') {
+            var table_set = $('#coin_premium_lists_table')[0]
+            var rowList = table_set.rows;
+            // console.log(rowList)
+            for (i=1; i<rowList.length; i++) {
+                var row = rowList[i];     //thead 부분을 제외하기 위해 i가 1부터 시작됩니다.
+                // var tdsNum = row.childElementCount;     //아래 for문에서 사용하기 위해 row 하위에 존재하는 td의 갯수를 구합니다.
 
-    //         var ticker_name = row.cells[0].innerHTML;
-    //         var ticker_spot_price = row.cells[1].innerHTML;
-    //         var ticker_future_price = row.cells[2].innerHTML;
-    //         var ticker_premium = row.cells[3].innerHTML;
-    //         // 만약 현물 가격, 선물가격, 김치 프리미엄이 0인 경우 행 숨김 처리 해주기 
-    //         if (ticker_spot_price==0 || ticker_future_price==0 || ticker_premium==0) {
-    //             row.style.display='none';
-    //         } else {  // 전부 있을 경우 다시 보여주기
-    //             row.style.display='';
-    //         }
+                var ticker_name = row.cells[0].innerHTML;
+                var ticker_spot_price = row.cells[1].innerHTML;
+                var ticker_future_price = row.cells[2].innerHTML;
+                var ticker_premium = row.cells[3].innerHTML;
+                // 만약 현물 가격, 선물가격, 김치 프리미엄이 0인 경우 행 숨김 처리 해주기 
+                if (ticker_spot_price==0 || ticker_future_price==0 || ticker_premium==0) {
+                    row.style.display='none';
+                } else {  // 전부 있을 경우 다시 보여주기
+                    row.style.display='';
+                }
 
-    //         // console.log(ticker_name, ticker_spot_price, ticker_future_price, ticker_premium)
-    //     };
-    // }) ();
+                // console.log(ticker_name, ticker_spot_price, ticker_future_price, ticker_premium)
+            };
+        };
+    }) ();
 
     // spot top 10 고정 정렬
     $('#spot_top_10_tr').children().each(function (column){
